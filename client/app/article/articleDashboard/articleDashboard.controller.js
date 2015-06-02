b96fb9ad244262b4e2fe578af18939cbd26a3e40
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-06-02 00:25:54 -0400

    adding cancel button, adding truncated article preview in dashboard

--- client/app/article/articleDashboard/articleDashboard.controller.js
+++ client/app/article/articleDashboard/articleDashboard.controller.js
@@ -5,6 +5,12 @@ angular.module('portfolioApp')
     // Grab the initial set of available articles
     $http.get('/api/articles').success(function(articles) {
       $scope.articles = articles;
+
+      $scope.articleTruncate = function (_content) {
+        // return _.chain(_content.split('')).take(2);
+        var trunc_length = 200 //
+        return _content.substring(0,trunc_length).concat((_content.length > trunc_length+1)?'...':''); // Take only 100 characters and then add ellipses
+      };
  
       // Update array with any new or deleted items pushed from the socket
       // socket.syncUpdates('article', $scope.articles, function(event, article, articles) {
