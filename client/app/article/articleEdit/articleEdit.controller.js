5ae6da24ecf3883ac4ab69a92d455d407a8e3eac
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-06-01 23:27:27 -0400

    edit and create articles now work

--- client/app/article/articleEdit/articleEdit.controller.js
+++ client/app/article/articleEdit/articleEdit.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('portfolioApp')
-  .controller('ArticleEditCtrl', function ($scope, $http) {
+  .controller('ArticleEditCtrl', function ($scope, $http, $stateParams, $location) {
     $scope.options = {
       site: [
         {'value': 1, 'text': 'All'},
@@ -23,9 +23,31 @@ angular.module('portfolioApp')
         {'value': 6, 'text': 'Motivation'},
       ]
     };
+
+    // Either we're editing an existing article or creating a new one, we can figure this out from params in url
+    $scope.articleID = (typeof $stateParams.article_id === 'undefined' || $stateParams.article_id === '') ? null : $stateParams.article_id;
+    $scope.articleExists = false; // Initialize article flag to either call a post or put later
+
+    // If articleID exists from URL bar, we call web services to grab info and populate
+    if(!!$scope.articleID){
+      $http.get('/api/articles/'+$scope.articleID).success(function(article) {
+        // If article is found, we the form with the existing article for edit
+        $scope.articleSite = $scope.options.site[article.site-1]; // DB index starts at 0, front end array starts at 0
+        $scope.articleType = $scope.options.type[article.type-1]; // DB index starts at 0, front end array starts at 0
+        $scope.articleCategory = article.category;
+        $scope.articleTitle = article.title;
+        $scope.articleContent = article.content;
+        $scope.articleExists = true; // article exists, so set flag so we can put instead of post
+      }).error( function (data, status, headers, config) {
+        $scope.articleExists = false;
+      });
+    }
+    // Either we have existing articleID and it's real, which will change below values later, or we don't and we need to use defaults
+    // Or the third case is that articleID exists but it's not in DB, so the $http.get will be a fail, so we'll have to use defaults so below is outside $http.get block for this reason
     $scope.articleSite = $scope.options.site[0];
     $scope.articleType = $scope.options.type[0];
     $scope.articleCategory = [1];
+    
 
     // Handles the selection of categories so we can POST to server as a simple arrayx
     $scope.toggleCategory = function (category_val) {
@@ -46,26 +68,43 @@ angular.module('portfolioApp')
   	$scope.createArticle = function ($event) {
   		$event.stopPropagation();
 
-      //POST to server to create new content
-      $http.post('/api/articles', {
-        title: $scope.articleTitle,
-        content: $scope.articleContent,
-        site: $scope.articleSite.value, // Note that this is an object so we'll have to explicitly refer to .value because server expecting it, otherwise validation error will pop
-        type: $scope.articleType.value, // Note that this is an object so we'll have to explicitly refer to .value because server expecting it, otherwise validation error will pop
-        category: $scope.articleCategory
+      var req = {
+        method: ($scope.articleExists)?'PUT':'POST', // Either a create new or an update, the routes are differentiated so server knows
+        url: ($scope.articleExists)?'/api/articles/'+$scope.articleID:'/api/articles', // same as above
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        data: {
+          title: $scope.articleTitle,
+          content: $scope.articleContent,
+          site: $scope.articleSite.value, // Note that this is an object so we'll have to explicitly refer to .value because server expecting it, otherwise validation error will pop
+          type: $scope.articleType.value, // Note that this is an object so we'll have to explicitly refer to .value because server expecting it, otherwise validation error will pop
+          category: $scope.articleCategory
+        }
+      }
+
+      $http(req).success(function(article, status, headers, config){
+        $scope.redirect = true;
+        $location.path('article/dashboard'); // Redirect to dashboard if success
+      }).error(function(data, status, headers, config){
+
       });
+
+      //POST to server to create new content
+      // $http.post('/api/articles', );
       // Reset view
       // $scope.articleTitle = '' // Clear input
       // $scope.articleContent = '' // Clear input
 
   	};
 
-    // If user leaves, ask if sure
-    $scope.$on('$locationChangeStart', function(event) {
-      debugger;
-      var answer = confirm("Are you sure you want to leave this page?")
-      if (!answer) {
-        event.preventDefault();
-      }
-    });
+    // If user leaves, ask if sure, NOTE: this requires $location service
+    // TODO: finish unsaved changes detection
+    // $scope.$on('$locationChangeStart', function(event) {
+    //   if ($scope.redirect) {return}; // Already saved so we don't need to confirm
+    //   var answer = confirm("Are you sure you want to leave this page?")
+    //   if (!answer) {
+    //     event.preventDefault();
+    //   }
+    // });
   });
--- client/app/article/articleEdit/articleEdit.js
+++ client/app/article/articleEdit/articleEdit.js
@@ -4,6 +4,11 @@ angular.module('portfolioApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('articleEdit', {
+        url: '/article/edit/:article_id',
+        templateUrl: 'app/article/articleEdit/articleEdit.html',
+        controller: 'ArticleEditCtrl'
+      })
+      .state('articleCreate', {
         url: '/article/edit',
         templateUrl: 'app/article/articleEdit/articleEdit.html',
         controller: 'ArticleEditCtrl'
