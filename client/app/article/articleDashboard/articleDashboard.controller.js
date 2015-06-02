a8683276865f7cb4d6f1852452c6b34f5edc2aa9
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-06-02 01:02:48 -0400

    truncation function before ng-repeat hits, adding last_updated into dashboard and styling

--- client/app/article/articleDashboard/articleDashboard.controller.js
+++ client/app/article/articleDashboard/articleDashboard.controller.js
@@ -4,14 +4,14 @@ angular.module('portfolioApp')
   .controller('ArticleDashboardCtrl', function($scope, $http) {
     // Grab the initial set of available articles
     $http.get('/api/articles').success(function(articles) {
-      $scope.articles = articles;
-
-      $scope.articleTruncate = function (_content) {
-        // return _.chain(_content.split('')).take(2);
-        var trunc_length = 200 //
-        return _content.substring(0,trunc_length).concat((_content.length > trunc_length+1)?'...':''); // Take only 100 characters and then add ellipses
-      };
- 
+      // Truncate content for each article so dashboard isn't cluttered
+      $scope.articles = articles.map(function (_article) {
+        var _content = _article.content;
+        var trunc_length = 200; // truncation length in characters
+        _article.content = _content.substring(0,trunc_length).concat((_content.length > trunc_length+1)?'...':''); // Take only 100 characters and then add ellipses
+        return _article;
+      });
+      
       // Update array with any new or deleted items pushed from the socket
       // socket.syncUpdates('article', $scope.articles, function(event, article, articles) {
       //   // This callback is fired after the articles array is updated by the socket listeners
