e872e30f9968e09c571752ef28acbc21c1eefa0a
Phong Thieu <pthieu@klick.com>
2015-06-05 14:37:18 -0400

    some space formatting and adding simple view to /article

--- client/app/article/article.controller.js
+++ client/app/article/article.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('portfolioApp')
-  .controller('ArticleCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ArticleCtrl', function($scope, $http) {
+    $http.get('/api/articles').success(function(articles) {
+      // Truncate content for each article so dashboard isn't cluttered
+      $scope.articles = articles.map(function(_article) {
+        var _content = _article.content;
+        var trunc_length = 200; // truncation length in characters
+        _article.content = _content.substring(0, trunc_length).concat((_content.length > trunc_length + 1) ? '...' : ''); // Take only 100 characters and then add ellipses
+        return _article;
+      });
+    });
   });
--- client/app/article/article.js
+++ client/app/article/article.js
@@ -3,6 +3,11 @@
 angular.module('portfolioApp')
   .config(function ($stateProvider) {
     $stateProvider
+    .state('articleFocus', {
+        url: '/article/:article_id',
+        templateUrl: 'app/article/article.html',
+        controller: 'ArticleCtrl'
+      })
       .state('article', {
         url: '/article',
         templateUrl: 'app/article/article.html',
