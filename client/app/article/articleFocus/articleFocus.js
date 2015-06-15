35d91078fec6b08519aad7f13e4e2dd80374bb8b
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-06-15 00:06:50 -0400

    adding in articleFocus title change

--- client/app/article/articleFocus/articleFocus.js
+++ client/app/article/articleFocus/articleFocus.js
@@ -6,6 +6,9 @@ angular.module('portfolioApp')
       .state('article.focus', {
         url: '/:article_stub', // note that because the state is a child state (i.e. has a dot), it will automatically append parent url section
         templateUrl: 'app/article/articleFocus/articleFocus.html',
-        controller: 'ArticleFocusCtrl'
+        controller: 'ArticleFocusCtrl',
+        resolve: {
+          $title: function ($stateParams) { return $stateParams.article_stub.replace(/-/gi, ' '); }
+        }
       });
   });
