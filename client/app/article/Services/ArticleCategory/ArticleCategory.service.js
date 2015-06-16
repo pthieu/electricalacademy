a944ce8f669dbde7db7a1b64ef756b1f915f2ee5
Phong Thieu <pthieu@klick.com>
2015-06-15 23:42:14 -0400

    cleaning up some comments

--- client/app/article/Services/ArticleCategory/ArticleCategory.service.js
+++ client/app/article/Services/ArticleCategory/ArticleCategory.service.js
@@ -2,9 +2,6 @@
 
 angular.module('portfolioApp')
   .factory('ArticleCategoryService', function ($http, $q) {
-    // Service logic
-    // ...
-
     var categories = [];
 
     var _getCategories = $http.get('/api/articles/category').error(function (data, status, headers, config) {}).then(function (_categories) {
@@ -21,7 +18,6 @@ angular.module('portfolioApp')
         if (categories.length > 0) {
             deferred.resolve(categories);
         } else {
-            // deferred.resolve(_getCategories);
             return _getCategories; //$http already returns a promise so we don't need to care about this
         }
         return deferred.promise;
