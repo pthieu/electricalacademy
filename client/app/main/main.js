a0d6f661d65d064ab70dd35924a34799bd466e7c
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-07-07 01:49:55 -0400

    temp meta desc

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,7 +8,8 @@ angular.module('electricalacademyApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl',
         resolve: {
-          $title: function () { return 'Home'; }
+          $title: function () { return 'Electrical Academy - Electrical Engineering and electronics learning and tutorials for free!'; },
+          $description: function () { return 'The best place to learn electronics or electrical engineering principles for free!'; }
         }
       });
   });
