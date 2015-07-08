2789749e4e05be509d326ee767148e2701f7ad62
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-07-08 01:18:20 -0400

    adding www sitemap

--- server/sitemap.js
+++ server/sitemap.js
@@ -15,9 +15,6 @@ var Article = require('./api/article/article.model');
 var Lesson = require('./api/lesson/lesson.model');
 
 module.exports = function(app) {
-  var articleURLs = [];
-  var lessonURLs = [];
-
   // sitemap object with hardcoded URLs
   var sitemap = sm.createSitemap({
     hostname: (config.env === 'production') ? 'http://' + config.hostname : 'http://localhost:' + config.port,
@@ -33,12 +30,29 @@ module.exports = function(app) {
     }]
   });
 
+  var sitemapWWW = sm.createSitemap({
+    hostname: (config.env === 'production') ? 'http://www.' + config.hostname : 'http://www.localhost:' + config.port,
+    cacheTime: 600000, // 600 sec - cache purge period 
+    urls: [{
+      url: '', // http://www.electricalacademy.com
+      changefreq: 'daily',
+      priority: 1
+    }, {
+      url: '/', // http://www.electricalacademy.com + '/'
+      changefreq: 'daily',
+      priority: 1
+    }]
+  });
+
   // Create a promise that gets triggered after multiple mongoose calls completed.
   Q.all([
       Article.find({}).exec(),
       Lesson.find({}).exec()
     ])
     .spread(function(articleResult, lessonResult) {
+      var articleURLs = [];
+      var lessonURLs = [];
+
       // iterate through results and push to URLS accordingly
       articleResult.forEach(function(article) {
         var t = new Date(article._created);
@@ -76,10 +90,64 @@ module.exports = function(app) {
         return coll;
       }, sitemap.urls);
     });
+  
+  // For some reason we need to split out the WWW to a different scope otherwise there will be conflicts
+  Q.all([
+      Article.find({}).exec(),
+      Lesson.find({}).exec()
+    ])
+    .spread(function(articleResult, lessonResult) {
+      var articleURLs = [];
+      var lessonURLs = [];
+
+      // iterate through results and push to URLS accordingly
+      articleResult.forEach(function(article) {
+        var t = new Date(article._created);
+        var articleDate = {
+          year: t.getUTCFullYear(),
+          month: t.getUTCMonth(),
+          day: t.getUTCDay()
+        };
+
+        // manually create date section for the url to articleFocus
+        // the "''" in the first and last index is to apply an additional '/' for the url
+        articleDate.url = ['', articleDate.year, articleDate.month, articleDate.day, ''].join('/');
+
+        articleURLs.push({
+          url: '/article' + articleDate.url + article.stub,
+          changefreq: 'monthly',
+          priority: 0.5
+        });
+      });
+      lessonResult.forEach(function(lesson) {
+        lessonURLs.push({
+          url: ['/lesson', lesson.stub].join('/'),
+          changefreq: 'monthly',
+          priority: 0.5
+        });
+      });
+
+      // Do the same for www version of site
+      sitemapWWW.urls = articleURLs.reduce(function(coll, item) {
+        coll.push(item);
+        return coll;
+      }, sitemapWWW.urls);
+      sitemapWWW.urls = lessonURLs.reduce(function(coll, item) {
+        coll.push(item);
+        return coll;
+      }, sitemapWWW.urls);
+    });
 
   app.route('/sitemap.xml')
     .get(function(req, res) {
       sitemap.toXML(function(xml) {
+        res.header('Content-Type', 'application/xml');
+        res.send(xml);
+      });
+    });
+  app.route('/sitemapwww.xml')
+    .get(function(req, res) {
+      sitemapWWW.toXML(function(xml) {
         res.header('Content-Type', 'application/xml');
         res.send(xml);
       });
